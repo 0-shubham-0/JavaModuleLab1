e6e6db805c7f1f8069bb2e4ecbd44d814632631a
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-09-16 15:28:05 +0530

    Atm

---
+++ src/ATM.java
@@ -0,0 +1,26 @@
+/*
+Pooja would like to withdraw X $US from an ATM.
+The cash machine will only accept the transaction if X
+is a multiple of 5, and Pooja's account balance has enough
+cash to perform the withdrawal transaction
+(including bank charges).
+For each successful withdrawal the bank charges 0.50 $US.
+Calculate Pooja's account balance after an
+ attempted transaction.
+*/
+public class ATM {
+    public static void main(String[] args) {
+        float x,am=10000;
+        x=500;
+        if(x % 5 == 0){
+            if(x>am-0.5f)
+                System.out.println("not enough money="+am);
+            else
+            {
+                am= am - x-0.5f;
+                System.out.println("Account balance= "+ am);}
+        }
+        else
+            System.out.println("withdraw value should be multiple of 5. Account balance= " + am);
+    }
+}
