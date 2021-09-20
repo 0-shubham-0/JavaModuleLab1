8d222848d92b55e352057f825c0acb7db9f25472
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-09-20 15:28:25 +0530

    use constructor

--- src/Student.java
+++ src/Student.java
@@ -7,7 +7,7 @@ class marks{
     double avg(){
         return (chem + cp + math + phys)/4;
     }
-    void mark(double a, double b,double c,double d){
+     marks (double a, double b,double c,double d){
         math = a;
         chem = b;
         phys = c;
@@ -17,12 +17,12 @@ class marks{
 
 public class Student {
     public static void main(String[] args){
-        marks m = new marks();
+        marks m = new marks(90,80,99,80);
 //        m.chem=90;
 //        m.cp=80;
 //        m.phys=99;
 //        m.math=70;
-          m.mark(90,50,80,99);
+
 
         double avg1 = m.avg();
         System.out.println("Your average is "+ avg1);
