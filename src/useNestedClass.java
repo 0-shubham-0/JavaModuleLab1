23e6dec255cc08f71fe8638b9112ff14f9a44d33
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-10-04 15:03:38 +0530

    JavaLab

---
+++ src/useNestedClass.java
@@ -0,0 +1,19 @@
+public class useNestedClass {
+    private int a=30;
+    class Inner{
+        void msg(){
+        System.out.println("a is "+a );
+    }
+    }
+    static void msg2(){
+        //System.out.println(a); can't use
+        System.out.println(30);
+    }
+    public static void main(String[] args){
+        useNestedClass obj=new useNestedClass();
+        useNestedClass.Inner in= obj.new Inner();
+        in.msg();
+        useNestedClass.msg2();
+        System.out.println();
+    }
+}
---
+++ src/useStaticVariables.java
@@ -0,0 +1,14 @@
+public class useStaticVariables {
+    //int count=0;
+    static int count=0;
+    useStaticVariables(){
+        count++;
+        System.out.println(count);
+    }
+    public static void main(String args[]){
+        useStaticVariables c1=new useStaticVariables();
+        useStaticVariables c2=new useStaticVariables();
+        useStaticVariables c3=new useStaticVariables();
+        System.out.println(count);
+    }
+}
