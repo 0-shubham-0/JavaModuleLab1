9cdffb3ab71ae1538e98d58f3121366e1bd62912
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-10-17 12:20:42 +0530

    JavaLab

---
+++ src/Inheritance/useFinal.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+class head{}
+class tail extends head{}
+
+final class head2{}
+//class tail2 extends head2{} can't make subclass of final
+public class useFinal {
+    public static void main(String []args){
+
+    }
+}
