5ebcea20a37ff7d31de54be1033da7698b0f5e02
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-10-18 16:51:10 +0530

    JavaLab

--- src/Inheritance/abstractClass.java
+++ src/Inheritance/abstractClass.java
@@ -2,6 +2,7 @@ package Inheritance;
 //makes it so that there is always a class which needs to be overridden
 abstract class shape{
     int l1,l2;
+    shape(){}
     abstract void property();//abstract method
     shape(int var1,int var2){//can have concrete methods
         l1=var1;
@@ -12,12 +13,14 @@ class square extends shape{
     square(int a){
         super(a,a);
     }
+    square(){}
     @Override
     //override property since its necessary or else will give error
     void property() {
         System.out.println("Area is "+l1*l2);
     }
 }
+class something extends square{} //abstract can't have multiple inheritance
 public class abstractClass {
     public static void main(String []args){
         square s1=new square(4);
